012778f7a7dc0e573b395b5b40098131ecb7859c
Justin Gasper <jmgasper@gmail.com>
2026-02-09 08:43:15 +1100

    Remove this check entirely

--- src/api/user/auth-flow.service.ts
+++ src/api/user/auth-flow.service.ts
@@ -239,14 +239,7 @@ export class AuthFlowService {
       );
       this.logger.log(`Published 'event.user.activated' event for ${userId}`);
 
-      // Send Welcome Email directly, matching legacy Java behavior
-      if (
-        CommonUtils.validateString(user.reg_source) &&
-        !user.reg_source.match(/^tcBusiness$/)
-      ) {
-        // The current Welcome mail should not be sent to customers (connect users).
-        await this.userService.notifyWelcome(userId, primaryEmailAddress);
-      }
+      await this.userService.notifyWelcome(userId, primaryEmailAddress);
 
       // assign a default user role
       await this.userService.assignDefaultUserRole(userId);
